f45072321bbc85a8d53b9271aceca060c69c96ba
Zachary Leeper <zachl@cs.utexas.edu>
2021-11-05 14:14:38 -0500

    fixed a typo in x86_isa

--- problems/structs_unions/generated_problem/struct_ex.h
+++ problems/structs_unions/generated_problem/struct_ex.h
@@ -1,6 +1,9 @@
 typedef struct node {
-	int *array_0[2];
+	struct node *n_0;
 	int *array_1[2];
-	short s_2;
-	int *array_3[2];
+	char c_2;
+	int value_3;
+	int value_4;
+	int *array_5[2];
+	struct node *n_6;
 } node_t, *nptr_t;
